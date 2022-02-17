c9879d52873c342a8e41b5c90c53fc0d150debd6
Dan Bowling <dan@danbowling.com>
2022-02-17 16:18:18 -0700

    fun

--- run_after_99-goodbye.sh
+++ run_after_99-goodbye.sh
@@ -1,3 +1,3 @@
 #!/bin/sh
 
-echo "ℹ️ Tip: Don't forget edit files in the repo. Run 'chezmoi cd' to get there."
+echo "Don't forget edit files in the repo. Run 'chezmoi cd' to get there." | cowsay | lolcat
