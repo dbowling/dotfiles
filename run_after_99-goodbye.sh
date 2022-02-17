d35da8d890a6530e841f5b163379bbe46d7f3900
Dan Bowling <dan@danbowling.com>
2022-02-16 17:57:27 -0700

    account for chezmoi install dir

---
+++ run_after_99-goodbye.sh
@@ -0,0 +1,4 @@
+#!/bin/sh
+
+echo "✨ You are up to date!"
+echo "ℹ️ Tip: Don't forget edit files in the repo. Run 'chezmoi cd' to get there."
--- run_once_before_install-00-prereqs.sh
+++ run_once_before_install-00-prereqs.sh
@@ -11,3 +11,16 @@ if [ ! "$(command -v ruby)" ]; then
     echo "⚠️ To install, you must have ruby." >&2
     exit 1
 fi
+
+if [ ! "$(command -v chezmoi)" ]; then
+    # this is the default location, which might not be in the path
+    [ -f $HOME/.bin/chezmoi ] && export PATH="$HOME/.bin:$PATH"
+fi
+
+# Check for chezmoi again after fix
+if [ ! "$(command -v chezmoi)" ]; then
+    echo "⚠️ chezmoi cannot be found" >&2
+    exit 1
+fi
+
+echo "👍 Initial checks look good!"
