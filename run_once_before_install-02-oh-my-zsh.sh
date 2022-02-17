57734e33091f9813f9586c895ad4752726ba28e1
Dan Bowling <dan@danbowling.com>
2022-02-17 10:28:50 -0700

    make zsh install idempotent

--- run_once_before_install-02-oh-my-zsh.sh
+++ run_once_before_install-02-oh-my-zsh.sh
@@ -3,4 +3,4 @@
 set -e # -e: exit on error
 
 echo "And now, Oh My ZSH!"
-/bin/sh -c "$(curl -fsSL https://raw.github.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
+[ ! -f $HOME/.oh-my-zsh/oh-my-zsh.sh ] && /bin/sh -c "$(curl -fsSL https://raw.github.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
