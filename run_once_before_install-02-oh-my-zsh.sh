771d5fd59cb2a4787d93c3234be2071bb64c31e3
Dan Bowling <dan@danbowling.com>
2022-02-17 10:32:54 -0700

    output nothing if ZSH is already installed

--- run_once_before_install-02-oh-my-zsh.sh
+++ run_once_before_install-02-oh-my-zsh.sh
@@ -2,5 +2,7 @@
 
 set -e # -e: exit on error
 
-echo "And now, Oh My ZSH!"
-[ ! -f $HOME/.oh-my-zsh/oh-my-zsh.sh ] && /bin/sh -c "$(curl -fsSL https://raw.github.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
+if [ ! -f "$HOME/.oh-my-zsh/oh-my-zsh.sh" ]; then
+    echo "Install Oh My ZSH!"
+    /bin/sh -c "$(curl -fsSL https://raw.github.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
+fi
