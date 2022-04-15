6599a91f904138a07df9da9beae771e4556b3594
Dan Bowling <daniel.bowling@umontana.edu>
2022-04-15 15:54:05 -0600

    adsf example

---
+++ run_after_01-asdf.sh
@@ -0,0 +1,13 @@
+#!/bin/sh
+
+set -e # -e: exit on error
+# an example of node, which I'm currently running via Volta
+# if [ ! -f "$HOME/.oh-my-zsh/oh-my-zsh.sh" ]; then
+    # echo "Add nodejs plugin"
+    # /bin/sh -c "$(asdf plugin add nodejs https://github.com/asdf-vm/asdf-nodejs.git)"
+    # echo "Install LTS"
+    # /bin/sh -c "$(asdf install nodejs lts)"
+    # echo "Configure LTS as global default"
+    #  /bin/sh -c "$(asdf global nodejs lts)"
+    
+# fi
