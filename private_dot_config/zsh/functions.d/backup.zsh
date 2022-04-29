3943e3bbf4e0772b13825ec1a57cd54c48bbe46e
Dan Bowling <daniel.bowling@umontana.edu>
2022-04-28 18:28:07 -0600

    show current repository settings

--- private_dot_config/zsh/functions.d/backup.zsh
+++ private_dot_config/zsh/functions.d/backup.zsh
@@ -5,17 +5,16 @@
 #   backup --dry-run            additional arg of --dry-run
 #   backup --dry-run --tag foo  multiple args are supported
 backup() {
-    export B2_ACCOUNT_ID="40cf006f2ecc"
-    export B2_ACCOUNT_KEY="001bce3aa993ad23176be6928b8bd4a128317a53c2"
-    export RESTIC_PASSWORD="macmouse"
-    export RESTIC_REPOSITORY="b2:work-backups:restic"
-
     export B2_CONCURRENT_CONNECTIONS="10"
 
+    echo "🚛 You will need to specify restic env variables:"
+    echo "🚛     B2_ACCOUNT_ID:     $B2_ACCOUNT_ID"
+    echo "🚛     B2_ACCOUNT_KEY:    $B2_ACCOUNT_KEY"
+    echo "🚛     RESTIC_REPOSITORY: $RESTIC_REPOSITORY"
     echo "🚛 Backing up ~/code with Restic... "
     echo "🚛 ℹ️  conncurrent B2 connections: $B2_CONCURRENT_CONNECTIONS"
     echo "🚛 ℹ️  additional arguments: $* " 
-    restic backup ~/code -o b2.connections=$B2_CONCURRENT_CONNECTIONS $* 
+    restic backup ~/code -o b2.connections=$B2_CONCURRENT_CONNECTIONS --exclude="node_modules" $* 
     echo "🚛 Snapshots... "
     restic snapshots --group-by host
 }
